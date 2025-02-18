a93448995a97a341445d1da7b25dcfc73028960f
coldBlood <85475503+nileshhaz@users.noreply.github.com>
2025-02-18 16:45:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,13 +3,28 @@ const handleForm = (e)=>{
 	e.preventDefault()
 	const age = document.getElementById("age").value
 	const name = document.getElementById("name").value
+	const button = document.getElementById("btn");
 
 	if(!age || !name){
 		manageAlert("Please enter valid details.")
-		return false
+		return 
 	}
-	if(age>18) manageAlert(`Welcome, ${name}. You can vote.`)
-	else manageAlert("Oh sorry . You aren't old enough.")
+
+	button.disabled = true;
+
+	new Promise((res,err)=>{
+		setTimeout(()=>{
+			if(age>18) res(`Welcome, ${name}. You can vote.`)
+			else err("Oh sorry . You aren't old enough.")
+		},4000)
+	}).then((mess)=>{
+		manageAlert(mess)
+	}).catch((err)=>{
+		manageAlert(err)
+	}).finally(() => {
+        button.disabled = false;
+    });
+	
 	document.getElementById("age").value=''
 	document.getElementById("name").value=''
 }

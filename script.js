d69d431a62c045fdcfe0d095e5b001f7c6f1bbcf
coldBlood <85475503+nileshhaz@users.noreply.github.com>
2025-02-18 16:30:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const handleForm = (e)=>{
+	e.preventDefault()
+	const age = document.getElementById("age").value
+	const name = document.getElementById("name").value
+
+	if(!age || !name){
+		manageAlert("Please enter valid details.")
+		return false
+	}
+	if(age>18) manageAlert(`Welcome, ${name}. You can vote.`)
+	else manageAlert("Oh sorry . You aren't old enough.")
+	document.getElementById("age").value=''
+	document.getElementById("name").value=''
+}
+const manageAlert=(e)=>{
+	alert(e)
+}
